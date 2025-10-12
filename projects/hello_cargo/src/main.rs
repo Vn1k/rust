ee98a7deb2d936ea027d4277dc44835698d15585
Vn1k <kevinken1912@icloud.com>
2025-10-12 15:24:49 +0700

    feat: 🎸 learn rust using cargo and normal way

---
+++ projects/hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, dunia!");
+}
+
+/*
+cargo new -> create new project
+cargo build -> build the project unoptimization and debug (if its still under developing)
+cargo build --release -> for final build of the project to be released with optimization (production things)
+cargo run -> build and run the project
+cargo check -> checking the build project without building it / running it
+*/
---
+++ projects/hello_world/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello World!");
+}
+
+/* 
+rustc to compile the file and running it with ./<name_of_the_file>
+
+! adalah macro dalam rust yang akan di convert sebelum mengcompile nya kedalam bahasa rust itu sendiri. 
+Tanpa ! maka itu akan memanggil fungsi bukan macro.
+*/
